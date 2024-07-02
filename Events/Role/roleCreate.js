9d58870f036c0edf61f7b6f753bf05f48a1eab72
yehia sakr <160256331+yehiaSAKR29@users.noreply.github.com>
2024-07-02 21:26:22 +0300

    Create roleCreate.js

---
+++ Events/Role/roleCreate.js
@@ -0,0 +1,25 @@
+const {EmbedBuilder} = require("discord.js");
+const {LOGS_CHANNEL} = require("../../config.json");
+
+module.exports = {
+  name: 'roleCreate',
+  async execute(role){
+    const logsChannel = LOGS_CHANNEL;
+    const channel = role.guild.channels.cache.get(logsChannel);
+
+    if(!channel){
+      return;
+    }
+
+
+    //logs created roles
+    const logsEmbed = new EmbedBuilder()
+      .setColor(0xff0000)
+      .setTitle("🎭 Role Created")
+      .addFields({name: "ID", value: `> ${role.id}`})
+      .setTimestamp()
+      .setFooter({text: 'Log System'})
+
+    channel.send({embeds: [logsEmbed]});
+  }
+}
