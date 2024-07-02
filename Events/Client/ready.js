427b5b32aad99703e21f6ec6a0057970b39035af
yehia sakr <160256331+yehiaSAKR29@users.noreply.github.com>
2024-07-02 10:35:54 +0300

    Organizing lines

--- Events/Client/ready.js
+++ Events/Client/ready.js
@@ -16,6 +16,7 @@ module.exports = {
 //registers slash commands
 async function registerSlashCommands(client, commands){
   const rest = new REST({version: '10'}).setToken(process.env['BOT_TOKEN']);
+  
   try{
     await rest.put(
       Routes.applicationGuildCommands(process.env['CLIENT_ID'], process.env['GUILD_ID']),
